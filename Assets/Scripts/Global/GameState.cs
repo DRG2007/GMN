1771f4214ceca122c4ca3cb52459ef5d4ecea4b4
RageBuster <136555235+RageBuster@users.noreply.github.com>
2024-04-26 08:51:22 -0400

    merging alt dev

---
+++ Assets/Scripts/Global/GameState.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+namespace GMN.Global
+{
+    public static class GameState
+    {
+        private static int level;
+
+        private static int hasCompletedTutorial;
+
+        private static int levelsCompleted;
+
+        private static int firstTimeRun;
+
+        public const string FirstTimeRunString = "FirstTimeRun";
+
+        public const string LevelString = "Level";
+
+        public const string HasCompletedTutorialString = "TutorialCompleted";
+
+        public const string levelsCompletedString = "LevelsCompleted";
+
+        public static int HasCompletedTutorial { get => hasCompletedTutorial;}
+        public static int FirstTimeRun { get => firstTimeRun;}
+        public static int LevelsCompleted { get => levelsCompleted;}
+        public static int Level { get => level;}
+
+        public static void Setup()
+        {
+            #if UNITY_EDITOR
+             firstTimeRun = 1;
+             hasCompletedTutorial = 0;
+             levelsCompleted = 0;
+             PlayerPrefs.SetInt(HasCompletedTutorialString, hasCompletedTutorial);
+             PlayerPrefs.SetInt(levelsCompletedString, levelsCompleted);
+             PlayerPrefs.SetInt(FirstTimeRunString, FirstTimeRun);
+
+
+             PlayerPrefs.Save();
+            #endif
+            if (PlayerPrefs.GetInt(FirstTimeRunString, 0) == 0)
+            {
+                firstTimeRun = 1;
+                hasCompletedTutorial = 0;
+                levelsCompleted = 0;
+                PlayerPrefs.SetInt(HasCompletedTutorialString, hasCompletedTutorial);
+                PlayerPrefs.SetInt(levelsCompletedString, levelsCompleted);
+                PlayerPrefs.SetInt(FirstTimeRunString, FirstTimeRun);
+
+
+                PlayerPrefs.Save();
+                
+            }
+        }
+
+        public static void CompleteTutorial()
+        {
+            if(PlayerPrefs.GetInt(FirstTimeRunString, 0) == 1)
+            {
+                hasCompletedTutorial = 1;
+                PlayerPrefs.SetInt(HasCompletedTutorialString, hasCompletedTutorial);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public static void CompleteLevel()
+        {
+            if(PlayerPrefs.GetInt(levelsCompletedString, 0) == level - 1)
+            {
+                levelsCompleted++;
+                PlayerPrefs.SetInt(levelsCompletedString, levelsCompleted);
+                PlayerPrefs.Save();
+            }
+        }
+
+
+    }
+}
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using GMN.Global;
+using static GMN.Global.GameState;
 public class StartButton : MonoBehaviour
 {
+    public GameObject gameObjecto;
 
-    public void PlayGame ()
+    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(PlayerPrefs.GetInt(HasCompletedTutorialString, 0) == 0)
+        {
+            gameObjecto.SetActive(true);
+            gameObject.SetActive(false);
+        }
     }
 
 }
